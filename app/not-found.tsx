fd7b5c88cd72975f4aedabfe3f91277128453a23
MaxymMelnychuk <maxymmelnychuk100@gmail.com>
2026-04-28 14:47:14 +0200

    fix: improve text visibility on 404 not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="h-screen flex flex-col items-center justify-center text-white">
       <h1 className="text-8xl font-light font-montserrat">404</h1>
       <p className="text-5xl font-light">Page Not Found</p>
     </div>
